2cb6b57072fd7dd16ac99b9e67ee66e0ede094aa
ximemolina <ximenamolinaportilla01@gmail.com>
2025-11-16 20:44:21 -0600

    Load Datawarehouse + correcciones dw

---
+++ etl/load/general.py
@@ -0,0 +1,220 @@
+import pandas as pd
+from configs.connections import get_dw_engine
+from sqlalchemy import text
+
+engine = get_dw_engine()
+
+''' -----------------------------------------------------------------------
+            Queries de SQL para cargar el DataWarehouse
+    ----------------------------------------------------------------------- ''' 
+
+query_insert_DimTime = """
+    DECLARE @FechaInicio DATE = '2024-01-01';
+    DECLARE @FechaFin    DATE = '2025-12-01';
+
+    ;WITH Fechas AS (
+        SELECT @FechaInicio AS Fecha
+        UNION ALL
+        SELECT DATEADD(DAY, 1, Fecha)
+        FROM Fechas
+        WHERE Fecha < @FechaFin
+    )
+    INSERT INTO dw.DimTiempo (
+        TiempoID, Fecha, Anio, Mes, Dia
+    )
+    SELECT 
+        CONVERT(INT, FORMAT(Fecha, 'yyyyMMdd')) AS TiempoID,
+        Fecha,
+        YEAR(Fecha)  AS Anio,
+        MONTH(Fecha) AS Mes,
+        DAY(Fecha)   AS Dia
+    FROM Fechas
+    OPTION (MAXRECURSION 0);
+"""
+
+query_insert_dimCliente_dw = """
+    INSERT INTO dw.DimCliente (
+        SourceSystem, 
+        SourceKey,
+        Email,
+        Nombre,
+        Genero,
+        Pais,
+        FechaCreacionID
+    )
+    SELECT
+        :sourceSystem,
+        :sourceKey,
+        :Email,
+        :Nombre,
+        :Genero,
+        :Pais,
+        TiempoID
+    FROM dw.DimTiempo
+    WHERE Fecha = :FechaCreacion;
+
+"""
+
+query_select_clientes_stg = """
+    SELECT
+        C.source_system AS sourceSystem
+        ,C.source_code AS sourceCode
+        ,C.cliente_email AS Email
+        , C.cliente_nombre AS Nombre
+        ,C.genero_norm AS Genero
+        , C.pais_raw AS Pais
+        , C.fecha_creado_dt AS  FechaCreacion
+    FROM
+        stg.clientes AS C
+"""
+
+query_select_map_producto = """
+    SELECT 
+        P.source_system AS SourceSystem,
+        P.source_code AS SourceKey,
+        P.nombre_norm AS Nombre,
+        P.categoria_norm AS Categoria,
+        P.es_servicio AS EsServicio
+    FROM
+         stg.map_producto AS P
+"""
+
+query_insert_dimCliente_dw = """
+    INSERT INTO dw.DimCliente (
+        SourceSystem, 
+        SourceKey,
+        Email,
+        Nombre,
+        Genero,
+        Pais,
+        FechaCreacionID
+    )
+    SELECT
+        :sourceSystem,
+        :sourceKey,
+        :Email,
+        :Nombre,
+        :Genero,
+        :Pais,
+        TiempoID
+    FROM dw.DimTiempo
+    WHERE Fecha = :FechaCreacion;
+
+"""
+
+query_insert_dimProducto_dw = """
+    INSERT INTO dw.DimProducto (
+        SKU, 
+        Nombre,
+        Categoria,
+        EsServicio,
+        SourceSystem,
+        SourceKey
+    )
+    VALUES (
+        :SKU, 
+        :Nombre,
+        :Categoria,
+        :EsServicio,
+        :SourceSystem,
+        :SourceKey)
+"""
+
+# Notar que ese query tiene NULLs, no los tendrían
+query_insert_factVentas=    """
+    INSERT INTO dw.FactVentas(
+        TiempoID,
+        ClienteID,
+        ProductoID,
+        Canal,
+        Fuente,
+        Cantidad,
+        PrecioUnitUSD,
+        TotalUSD,
+        MonedaOriginal,
+        PrecioUnitOriginal,
+        TotalOriginal)
+    SELECT
+        T.TiempoID,
+        C.ClienteID,
+        P.ProductoID,
+        O.canal_raw,
+        O.source_system,
+        O.cantidad_num,
+        O.precio_unit_num,
+        O.total_num,
+        O.moneda,
+        O.precio_unit_raw,
+        O.total_raw
+    FROM
+        dw.DimTiempo AS T
+        ,stg.orden_items AS O
+    INNER JOIN dw.DimCliente AS C
+    ON O.cliente_key = C.SourceKey
+    INNER JOIN dw.DimProducto AS P
+    ON O.source_code_prod = P.SourceKey
+    WHERE
+        T.Fecha = O.fecha_dt
+"""
+
+def load_dim_tiempo():
+
+    # Cargar DimTiempo desde 2024-01-01 hasta 2025-12-31
+    with engine.begin() as conn:
+        conn.exec_driver_sql(query_insert_DimTime)
+
+def get_clientes_stg():
+    with engine.connect() as conn:
+        result = conn.execute(text(query_select_clientes_stg))
+        clientes_stg = result.fetchall()
+    return clientes_stg
+
+def load_dim_cliente():
+
+    clientes_stg = get_clientes_stg()
+
+    for cliente in clientes_stg:
+        with engine.connect() as conn:
+                conn.execute(text(query_insert_dimCliente_dw), {
+                        'sourceSystem':cliente.sourceSystem, 
+                        'sourceKey': cliente.sourceCode,
+                        'Email': cliente.Email, 
+                        'Nombre': cliente.Nombre, 
+                        'Genero': cliente.Genero, 
+                        'Pais': cliente.Pais,
+                        'FechaCreacion': cliente.FechaCreacion
+                    })
+                conn.commit()
+                
+def get_map_productos():
+    with engine.connect() as conn:
+        result = conn.execute(text(query_select_map_producto))
+        productos_stg = result.fetchall()
+    return productos_stg
+
+
+def load_dim_producto():
+
+    productos_stg = get_map_productos()
+
+    for producto in productos_stg:
+        with engine.connect() as conn:
+                conn.execute(text(query_insert_dimProducto_dw), {
+                        'SourceSystem':producto.SourceSystem, 
+                        'SourceKey': producto.SourceKey,
+                        'SKU': producto.SourceKey, 
+                        'Nombre': producto.Nombre, 
+                        'Categoria': producto.Categoria,
+                        'EsServicio': producto.EsServicio,
+                    })
+                conn.commit()
+
+def load_fact_ventas():
+    with engine.begin() as conn:
+        conn.exec_driver_sql(query_insert_factVentas)    
+
+def load_datawarehouse():
+    load_dim_tiempo()
+    load_dim_cliente()
+    load_dim_producto()
+    load_fact_ventas()
--- etl/main.py
+++ etl/main.py
@@ -1,9 +1,39 @@
 from extract.mongo import extract_mongo
 from transform.mongo import transform_mongo
-
+from load.general import load_datawarehouse
 
 if __name__ == "__main__":
     
-    # ETL mongo
-    #objetos_mongo = extract_mongo()
-    #transform_mongo(objetos_mongo[0], objetos_mongo[1], objetos_mongo[2])
+    # Extracciones de bases de datos
+    objetos_mongo = extract_mongo()
+
+    # Transformaciones de datos
+    transform_mongo(objetos_mongo[0], objetos_mongo[1], objetos_mongo[2])
+
+    # Carga general del datawarehouse
+    load_datawarehouse()
+
+    #pd: aun no se aplican los tipos de cambios pq no se como está el estado del job en stg.orden_items
+
+    # Función para resetear el DataWarehouse (eliminar datos cargados de prueba)
+    def reset_datawarehouse():
+        from sqlalchemy import text
+        from configs.connections import get_dw_engine
+        engine = get_dw_engine()
+        sql = """
+            delete from dw.FactVentas
+            delete from dw.DimProducto
+            delete from dw.DimCliente
+            delete from dw.DimTiempo
+            delete from stg.map_producto
+            delete from stg.orden_items
+            delete from stg.clientes
+        """
+
+        with engine.begin() as conn:
+            conn.execute(text(sql))
+
+    #reset_datawarehouse()
+
+
+    
